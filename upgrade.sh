d8a3fff6a96b6d937c0ea72d010e28db2daac16b
Andrew Tait Gehrhardt <atgehrhardt@gmail.com>
2024-01-17 15:58:44 -0500

    Added konsole install

--- upgrade.sh
+++ upgrade.sh
@@ -8,3 +8,6 @@ sudo frzr-unlock
 
 # Install ROG specific ChimeraOS build
 sudo frzr-deploy rog-ally-gaming/chimeraos:unstable
+
+# Install Konsole as this build doesn't include a console application
+sudo pacman -S konsole
